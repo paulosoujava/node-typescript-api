f011e9a654ad9d6024012fce9f03265fd89834fc
Paulo jorge <paulosoujava@gmail.com>
2020-08-10 20:02:20 -0300

    add jest typeof

---
+++ src/clients/__test__/stromGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from "@src/clients/stormGlass";
+import axios from "axios";
+import stormGlassWeather3HoursFixture from "@test/fixtures/stormglass_weather_3_hours.json";
+import stormGlassNormalized3HoursFixture from "@test/fixtures/stormglass_normalized_response_3_hours.json";
+
+jest.mock("axios");
+
+describe("StormGlass client", () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  it("should return the normalized forecast front the StormGlass service", async () => {
+    const lat = -33.898989;
+    const lng = 1519899;
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
